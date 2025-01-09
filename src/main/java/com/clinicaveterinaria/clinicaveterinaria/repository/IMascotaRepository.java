180df3077484abb8286e6b72c67ba3fbf427d384
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-01-09 18:10:08 -0300

    Added repositories

---
+++ src/main/java/com/clinicaveterinaria/clinicaveterinaria/repository/IMascotaRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.clinicaveterinaria.clinicaveterinaria.repository;
+
+import com.clinicaveterinaria.clinicaveterinaria.model.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMascotaRepository extends JpaRepository<Mascota, Long> {
+
+}
